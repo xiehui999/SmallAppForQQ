b1d8a1292a05c82dc35590471275d252006e43e1
xiehui <xiehui0902@gmail.com>
2016-09-26 09:54:45 +0800

    Add Data in message.js file.
    Add Data in message.js file.

--- pages/message/message.js
+++ pages/message/message.js
@@ -1,6 +1,63 @@
 Page({
   data:{
     // text:"这是一个页面"
+    urls:[
+      {
+        title:"马云",
+        url:"http://img1.3lian.com/gif/more/11/201212/0d1252b54be4f2d240b6b7fe4ed35054.jpg",
+        message:"什么鬼，我有支付宝"
+      },
+      {
+        title:"马化腾",
+        url:"http://img1.3lian.com/gif/more/11/201212/cd1d745ed855bef27f47c8aff0786067.jpg",
+        message:"哈哈哈，我要一统江湖啦，分分钟下线你的应用"
+        },
+      {
+        title:"李彦宏",
+        url:"http://img1.3lian.com/gif/more/11/201212/c011f2b2ab1a10d79fe931a786503d03.jpg",
+         message:"抄袭我的吧，我早都做过了"
+         },
+      {
+        title:"雷军",
+        url:"http://img1.3lian.com/gif/more/11/201212/cf9d56d05c0cbc1edbf21cf64ad9c341.jpg",
+        message:"微信给你下线，看你还嘚瑟"
+        },
+      {
+        title:"前端开发者",
+        url:"http://img1.3lian.com/gif/more/11/201212/04b335ead07530a6188a27549fad1a68.jpg",
+        message:"我们的春天来啦，哈哈哈"
+        },
+      {
+        title:"微信小程序",
+        url:"http://img1.3lian.com/gif/more/11/201212/e10e8faa201671b5a2e8a6146f80d99e.jpg",
+        message:"谁有内测资格啊啊，300万买一个"
+        },
+      {
+        title:"小程序",
+        url:"http://img1.3lian.com/gif/more/11/201212/3fab6379b6ef53e7c17a1cc772eed0d6.jpg",
+        message:"这个IDE,把自动化全整进去了，都不要配置了"
+        },
+      {
+        title:"微信应用号开发群",
+        url:"http://img1.3lian.com/gif/more/11/201212/8f3762fb4d791410ec970baa2641344d.jpg",
+         message:"开发的局限性太大了"
+         },
+      {
+        title:"后台",
+        url:"http://img1.3lian.com/gif/more/11/201212/411cffabcd6e4474b48f1ecac76e1b3f.jpg",
+        message:"不看好小程序"
+        },
+      {
+        title:"闯天下",
+        url:"http://img1.3lian.com/gif/more/11/201212/04378257474004fcdd62f619d724db0c.jpg",
+        message:"微信太强大了"
+        },
+      {
+        title:"小程序",
+        url:"http://img1.3lian.com/gif/more/11/201212/58ec7c89b16c529fc9d0dc440a72e14d.jpg",
+         message:"有快来看直播啦，学习开始啦"
+         }
+    ]
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
