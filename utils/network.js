840f8a3d04f2f1f49383bc4a5f759db590af0724
xiehui999 <xiehui0902@163.com>
2017-07-09 21:50:04 +0800

    文章网络请求简单封装
    文章网络请求简单封装

---
+++ utils/network.js
@@ -0,0 +1,47 @@
+function request(url, params, success, fail) {
+  this.requestLoading(url, params, "", success, fail)
+}
+function requestLoading(url, params, message, success, fail) {
+  console.log(params)
+  wx.showNavigationBarLoading()
+  if (message != "") {
+    wx.showLoading({
+      title: message,
+    })
+  }
+  wx.request({
+    url: url,
+    data: params,
+    header: {
+      //'Content-Type': 'application/json'
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    method: 'POST',
+    success: function (res) {
+      //console.log(res.data)
+      wx.hideNavigationBarLoading()
+      if (message != "") {
+        wx.hideLoading()
+      }
+      if (res.statusCode == 200) {
+        success(res.data)
+      } else {
+        fail()
+      }
+    },
+    fail: function (res) {
+      wx.hideNavigationBarLoading()
+      if (message != "") {
+        wx.hideLoading()
+      }
+      fail()
+    },
+    complete: function (res) {
+
+    },
+  })
+}
+module.exports = {
+  request: request,
+  requestLoading: requestLoading,
+}
