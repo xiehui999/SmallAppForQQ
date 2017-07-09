af69c016f9c3a1bfb3b3baa0d085c03c58502e83
xiehui999 <xiehui0902@163.com>
2017-07-09 21:27:20 +0800

    文章菜单按钮实现
    文章菜单按钮实现

---
+++ pages/study/directory.js
@@ -0,0 +1,138 @@
+// directory.js
+var animation
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menuType: 0,
+    begin: null,
+    status:1,
+    end: null,
+    isVisible: false,
+    animationData: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 初始化动画变量
+    var animation = wx.createAnimation({
+      duration: 500,
+      transformOrigin: "50% 50%",
+      timingFunction: 'ease',
+    })
+    this.animation = animation;
+  },
+  // 显示
+  showMenuTap: function (e) {
+    console.log('selectState')
+    //获取点击菜单的类型 1点击状态 2点击时间 
+    var menuType = e.currentTarget.dataset.type
+    // 如果当前已经显示，再次点击时隐藏
+    if (this.data.isVisible == true) {
+      this.startAnimation(false, -200)
+      return
+    }
+    this.setData({
+      menuType: menuType
+    })
+    this.startAnimation(true, 0)
+  },
+  hideMenuTap: function (e) {
+    this.startAnimation(false, -200)
+  },
+  // 执行动画
+  startAnimation: function (isShow, offset) {
+    var that = this
+    var offsetTem
+    if (offset == 0) {
+      offsetTem = offset
+    } else {
+      offsetTem = offset + 'rpx'
+    }
+    this.animation.translateY(offset).step()
+    this.setData({
+      animationData: this.animation.export(),
+      isVisible: isShow
+    })
+  },
+  // 选择状态按钮
+  selectState: function (e) {
+    console.log('selectState1')
+    this.startAnimation(false, -200)
+    var status = e.currentTarget.dataset.status
+    this.setData({
+      status: status
+    })
+    console.log(this.data)
+
+  },
+  // 日志选择
+  bindDateChange: function (e) {
+    console.log(e)
+    if (e.currentTarget.dataset.type == 1) {
+      this.setData({
+        begin: e.detail.value
+      })
+    } else if (e.currentTarget.dataset.type == 2) {
+      this.setData({
+        end: e.detail.value
+      })
+    }
+  },
+  sureDateTap: function () {
+    this.data.pageNo = 1
+    this.startAnimation(false, -200)
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
