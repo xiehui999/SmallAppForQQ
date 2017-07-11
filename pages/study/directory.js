34b74b2750c46a988d864ad76c8be87b23ede598
xiehui <xiehui0902@gmail.com>
2017-07-11 13:27:30 +0800

    文章省市县三级联动地址选择器
    文章省市县三级联动地址选择器

--- pages/study/directory.js
+++ pages/study/directory.js
@@ -1,17 +1,33 @@
 // directory.js
+var address = require('../../utils/city.js')
 var animation
 Page({
 
   /**
    * 页面的初始数据
+   * 当前    provinces:所有省份
+   * citys选择省对应的所有市,
+   * areas选择市对应的所有区
+   * provinces：当前被选中的省
+   * city当前被选中的市
+   * areas当前被选中的区
    */
   data: {
     menuType: 0,
     begin: null,
-    status:1,
+    status: 1,
     end: null,
     isVisible: false,
-    animationData: {}
+    animationData: {},
+    animationAddressMenu: {},
+    addressMenuIsShow: false,
+    value: [0, 0, 0],
+    provinces: [],
+    citys: [],
+    areas: [],
+    province: '',
+    city: '',
+    area: ''
   },
 
   /**
@@ -25,6 +41,14 @@ Page({
       timingFunction: 'ease',
     })
     this.animation = animation;
+    // 默认联动显示北京
+    var id = address.provinces[0].id
+    this.setData({
+      provinces: address.provinces,
+      citys: address.citys[id],
+      areas: address.areas[address.citys[id][0].id],
+    })
+    console.log(this.data)
   },
   // 显示
   showMenuTap: function (e) {
@@ -58,6 +82,7 @@ Page({
       animationData: this.animation.export(),
       isVisible: isShow
     })
+    console.log(that.data)
   },
   // 选择状态按钮
   selectState: function (e) {
@@ -134,5 +159,79 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  // 点击所在地区弹出选择框
+  selectDistrict: function (e) {
+    var that = this
+    console.log('111111111')
+    if (that.data.addressMenuIsShow) {
+      return
+    }
+    that.startAddressAnimation(true)
+  },
+  // 执行动画
+  startAddressAnimation: function (isShow) {
+    console.log(isShow)
+    var that = this
+    if (isShow) {
+      that.animation.translateY(0 + 'vh').step()
+    } else {
+      that.animation.translateY(40 + 'vh').step()
+    }
+    that.setData({
+      animationAddressMenu: that.animation.export(),
+      addressMenuIsShow: isShow,
+    })
+  },
+  // 点击地区选择取消按钮
+  cityCancel: function (e) {
+    this.startAddressAnimation(false)
+  },
+  // 点击地区选择确定按钮
+  citySure: function (e) {
+    var that = this
+    var city = that.data.city
+    var value = that.data.value
+    that.startAddressAnimation(false)
+    // 将选择的城市信息显示到输入框
+    var areaInfo = that.data.provinces[value[0]].name + ',' + that.data.citys[value[1]].name + ',' + that.data.areas[value[2]].name
+    that.setData({
+      areaInfo: areaInfo,
+    })
+  },
+  hideCitySelected: function (e) {
+    console.log(e)
+    this.startAddressAnimation(false)
+  },
+  // 处理省市县联动逻辑
+  cityChange: function (e) {
+    console.log(e)
+    var value = e.detail.value
+    var provinces = this.data.provinces
+    var citys = this.data.citys
+    var areas = this.data.areas
+    var provinceNum = value[0]
+    var cityNum = value[1]
+    var countyNum = value[2]
+    if (this.data.value[0] != provinceNum) {
+      var id = provinces[provinceNum].id
+      this.setData({
+        value: [provinceNum, 0, 0],
+        citys: address.citys[id],
+        areas: address.areas[address.citys[id][0].id],
+      })
+    } else if (this.data.value[1] != cityNum) {
+      var id = citys[cityNum].id
+      this.setData({
+        value: [provinceNum, cityNum, 0],
+        areas: address.areas[citys[cityNum].id],
+      })
+    } else {
+      this.setData({
+        value: [provinceNum, cityNum, countyNum]
+      })
+    }
+    console.log(this.data)
+  },
+
 })
